0b5adbcc8db244a6a406d1da7e271a8fd308018b
Semen Zhukov <semaqwinnert@gmail.com>
2021-07-23 22:54:26 +0300

    Exstends JPA redact

--- Наследование в JPA/extends-jpa/src/test/java/EntityExtendsEntity/OneTableAllClass/AsiaManTest.java
+++ Наследование в JPA/extends-jpa/src/test/java/EntityExtendsEntity/OneTableAllClass/AsiaManTest.java
@@ -1,71 +1,57 @@
 package EntityExtendsEntity.OneTableAllClass;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.Query;
-import javax.transaction.Transactional;
 
 public class AsiaManTest {
     EntityManagerFactory entityManagerFactory;
     AsiaMan asiaMan;
+    EntityManager entityManager;
+
     @Before
     public void setUp(){
         asiaMan = new AsiaMan();
         asiaMan.setName("Alik");
         asiaMan.setNational("I'm Asian Man ");
         entityManagerFactory = Persistence.createEntityManagerFactory("ru.easyjava.data.jpa.hibernate");
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
         entityManager = entityManagerFactory.createEntityManager();
-
         entityManager.getTransaction().begin();
         entityManager.persist(asiaMan);
+        entityManager.flush();
         entityManager.getTransaction().commit();
         entityManager.close();
-
     }
 
-
-    public AsiaMan getAsiaManTest(){
+    @Test
+    public void getAsiaManTest(){
         System.out.println("getAsianTest");
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager = entityManagerFactory.createEntityManager();
         entityManager.getTransaction().begin();
-        AsiaMan from_asiaMan = (AsiaMan) entityManager.createQuery("from AsiaMan");
+         entityManager.createQuery("from AsiaMan").getResultList().forEach(asiaMan->{
+             System.out.println(asiaMan);
+         });
         entityManager.getTransaction().commit();
-
-
-        //.forEach(asiaMan->{
-        //  System.out.println("ID:" + ((AsiaMan) asiaMan).getId());
-        //System.out.println("Name:" +((AsiaMan) asiaMan).getName());
-        //System.out.println("National:" +((AsiaMan) asiaMan).getNational());
-        // System.out.println();
-        //});
-        entityManager.close();
-        return from_asiaMan;
     }
     @Test
-
     public void refreshAsiaManTest(){
-        asiaMan = getAsiaManTest();
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        asiaMan = entityManager.find(AsiaMan.class, asiaMan);
+        entityManager.remove(asiaMan);
         asiaMan.setName("Anton");
         asiaMan.setName("I'm New Asia Man ");
+        entityManager.persist(asiaMan);
         entityManager.getTransaction().begin();
+        entityManager.remove(asiaMan);
         entityManager.merge(asiaMan);
         entityManager.refresh(asiaMan);
+        entityManager.flush();
         entityManager.getTransaction().commit();
-        getAsiaManTestTwo();
-        entityManager.close();
     }
     @Test
     public void getAsiaManTestTwo(){
         System.out.println("getAsianTest Two");
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
         entityManager.getTransaction().begin();
         entityManager.createQuery("from AsiaMan").getResultList().forEach(asiaMan->{
             System.out.println("ID:" + ((AsiaMan) asiaMan).getId());
@@ -74,13 +60,43 @@ public class AsiaManTest {
             System.out.println();
         });
         entityManager.getTransaction().commit();
-        entityManager.close();
     }
+
+    /**
+     * entityManagerFactory = Persistence.createEntityManagerFactory("ru.easyjava.data.jpa.hibernate");
+     *
+     *         //New op
+     *         Operation op = new Operation();
+     *
+     *         op.setId(1L);
+     *         op.setAccountId(100500);
+     *         op.setAmount(BigDecimal.TEN);
+     *         op.setTimestamp(ZonedDateTime.now());
+     *         op.setDescription("Test operation");
+     *         op.setOpCode(9000);
+     *
+     *         EntityManager em = entityManagerFactory.createEntityManager();
+     *         em.getTransaction().begin();
+     *         em.persist(op); // op is MANAGED now
+     *         em.flush();
+     *
+     *         em.remove(op); // op is REMOVED now
+     *         em.persist(op); // op is MANAGED now
+     *         op.setDescription("New operation name.");
+     *         em.getTransaction().commit();
+     *         em.close(); // op is DETACHED now
+     *
+     *         //Merge back
+     *         em = entityManagerFactory.createEntityManager();
+     *         em.getTransaction().begin();
+     *         op = em.merge(op); // op is MANAGED now
+     *         em.refresh(op); // OP is still MANAGED and reloaded from the DB
+     *         em.getTransaction().commit();
+     *         em.clear(); // op is DETACHED now
+     */
 
     @Test
     public void afterResultTest(){
         System.out.println("afterResultTest");
     }
-
-
 }
--- Наследование в JPA/extends-jpa/src/test/java/EntityExtendsEntity/OneTableAllClass/AsiaManTestTwo.java
+++ Наследование в JPA/extends-jpa/src/test/java/EntityExtendsEntity/OneTableAllClass/AsiaManTestTwo.java
@@ -1,7 +1,86 @@
 package EntityExtendsEntity.OneTableAllClass;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import static org.junit.Assert.*;
 
 public class AsiaManTestTwo {
+    EntityManagerFactory entityManagerFactory;
+    //AsiaMan asiaMan;
+    //EntityManager entityManager;
 
+    @Test
+    public void setUp(){
+        AsiaMan asiaMan = new AsiaMan();
+        asiaMan.setName("Alik");
+        asiaMan.setNational("I'm Asian Man ");
+        entityManagerFactory = Persistence.createEntityManagerFactory("ru.easyjava.data.jpa.hibernate");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.persist(asiaMan);
+        System.out.println(asiaMan);
+        entityManager.flush();
+        entityManager.remove(asiaMan);
+        entityManager.persist(asiaMan);
+        entityManager.createQuery("from AsiaMan ").getResultList().forEach(System.out::println);
+
+
+
+
+        asiaMan.setName("Semen");
+        asiaMan.setNational("Asia");
+        entityManager.getTransaction().commit();
+        entityManager.close();
+
+
+
+        entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        asiaMan = entityManager.merge(asiaMan);
+        entityManager.refresh(asiaMan);
+        entityManager.createQuery("from AsiaMan ").getResultList().forEach(System.out::println);
+
+        entityManager.getTransaction().commit();
+        entityManager.clear();
+        /**
+         *  /**
+         *      * entityManagerFactory = Persistence.createEntityManagerFactory("ru.easyjava.data.jpa.hibernate");
+         *      *
+         *      *         //New op
+         *      *         Operation op = new Operation();
+         *      *
+         *      *         op.setId(1L);
+         *      *         op.setAccountId(100500);
+         *      *         op.setAmount(BigDecimal.TEN);
+         *      *         op.setTimestamp(ZonedDateTime.now());
+         *      *         op.setDescription("Test operation");
+         *      *         op.setOpCode(9000);
+         *      *
+         *      *         EntityManager em = entityManagerFactory.createEntityManager();
+         *      *         em.getTransaction().begin();
+         *      *         em.persist(op); // op is MANAGED now
+         *      *         em.flush();
+         *      *
+         *      *         em.remove(op); // op is REMOVED now
+         *      *         em.persist(op); // op is MANAGED now
+         *      *         op.setDescription("New operation name.");
+         *      *         em.getTransaction().commit();
+         *      *         em.close(); // op is DETACHED now
+         *      *
+         *      *         //Merge back
+         *      *         em = entityManagerFactory.createEntityManager();
+         *      *         em.getTransaction().begin();
+         *      *         op = em.merge(op); // op is MANAGED now
+         *      *         em.refresh(op); // OP is still MANAGED and reloaded from the DB
+         *      *         em.getTransaction().commit();
+         *      *         em.clear(); // op is DETACHED now
+         *      */
+
+    }
 }
